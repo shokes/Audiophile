f901c52ea5e71a5e47461ac50ec8e28d2962a12b
Oshoke Oyati <oyatioshoke@yahoo.com>
2023-03-06 10:29:04 +0100

    cart component

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -6,7 +6,7 @@ import Button from '../Button';
 
 const Cart = () => {
   return (
-    <div className='text-black bg-white py-[31px]  px-[33px] rounded-lg shadow-lg w-[377px] '>
+    <div className='text-black bg-white py-[31px]  px-[33px] rounded-lg shadow-lg w-[377px]'>
       <div className='flex items-center justify-between mb-8'>
         <div>
           <Typography as='h6' weight='font-bold' transform='uppercase'>
@@ -25,20 +25,68 @@ const Cart = () => {
           alt='headphones'
           className='rounded-lg mr-4'
         />
-        <div className='flex flex-col'>
-          <span>
-            <Typography as='xsmall' weight='font-bold'>
-              XX99 MK II
-            </Typography>
-          </span>
-          <span className='opacity-50'>
-            <Typography as='p2' weight='font-bold'>
-              $2,999
-            </Typography>
-          </span>
+        <div className='flex justify-between w-full'>
+          <div className='flex flex-col'>
+            <span>
+              <Typography as='xsmall' weight='font-bold'>
+                XX99 MK II
+              </Typography>
+            </span>
+            <span className='opacity-50'>
+              <Typography as='p2' weight='font-bold'>
+                $2,999
+              </Typography>
+            </span>
+          </div>
+          <div className='bg-brandGray-200 px-[52px] py-[15px] '>1</div>
         </div>
-        <div className='bg-brandGray-200 px-[52px] py-[15px] justify-self-end'>
-          1
+      </div>
+      <div className='flex mb-6 items-center'>
+        <Image
+          src={headphones}
+          width={64}
+          height={64}
+          alt='headphones'
+          className='rounded-lg mr-4'
+        />
+        <div className='flex justify-between w-full'>
+          <div className='flex flex-col'>
+            <span>
+              <Typography as='xsmall' weight='font-bold'>
+                XX99
+              </Typography>
+            </span>
+            <span className='opacity-50'>
+              <Typography as='p2' weight='font-bold'>
+                $2,999
+              </Typography>
+            </span>
+          </div>
+          <div className='bg-brandGray-200 px-[52px] py-[15px] '>1</div>
+        </div>
+      </div>
+      <div className='flex mb-6 items-center'>
+        <Image
+          src={headphones}
+          width={64}
+          height={64}
+          alt='headphones'
+          className='rounded-lg mr-4'
+        />
+        <div className='flex justify-between w-full'>
+          <div className='flex flex-col'>
+            <span>
+              <Typography as='xsmall' weight='font-bold'>
+                XX99 MK II
+              </Typography>
+            </span>
+            <span className='opacity-50'>
+              <Typography as='p2' weight='font-bold'>
+                $2,999
+              </Typography>
+            </span>
+          </div>
+          <div className='bg-brandGray-200 px-[52px] py-[15px] '>1</div>
         </div>
       </div>
       <div className='flex justify-between mb-8'>
