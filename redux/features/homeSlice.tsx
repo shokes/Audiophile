61985b24de8f3bf1a71d0cd76fad04b4faf8011e
Oshoke Oyati <oyatioshoke@yahoo.com>
2023-03-12 18:44:23 +0100

    redux

--- redux/features/homeSlice.tsx
+++ redux/features/homeSlice.tsx
@@ -4,31 +4,11 @@ const initialState = {
   cart: [],
 };
 
-// export const getCryptos: any = createAsyncThunk(
-//   'cryptos/getCryptos',
-//   async () => {
-//     return fetch(requests.cryptoList)
-//       .then((resp) => resp.json())
-//       .catch((err) => console.log(err));
-//   }
-// );
-
 const homeSlice = createSlice({
   name: 'home',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
-    // builder.addCase(getCryptos.pending, (state) => {
-    //   state.loading = true;
-    // }),
-    //   builder.addCase(getCryptos.fulfilled, (state, action) => {
-    //     state.loading = false;
-    //     state.cryptos = action.payload;
-    //   }),
-    //   builder.addCase(getCryptos.rejected, (state) => {
-    //     state.loading = false;
-    //   });
-  },
+  extraReducers: (builder) => {},
 });
 
 // export const {
